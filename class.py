11f0c87a260e4036538082c060bdd1829deb6d6b
Yashwanth560 <nareddyyashwanthreddy@gmail.com>
2024-07-18 14:55:40 +0530

    New file

---
+++ class.py
@@ -0,0 +1,28 @@
+class Animal:
+    def __init__(self,a,b):
+        self.height=a
+        self.weight=b
+
+    def display(self):
+        print(self.height,self.weight)
+    def speak(self):
+        print("speak")
+
+class Dog(Animal):
+    def speak(self):
+        print("Bow Bow!")
+
+class Cat(Animal):
+    def speak(self):
+         super().speak()
+         print("meow Meow!")
+
+obj1=Cat(1,22)
+obj2=Dog(3,35)
+print(obj2.height)
+obj1.display()
+obj2.speak()
+obj1.speak()
+
+
+
